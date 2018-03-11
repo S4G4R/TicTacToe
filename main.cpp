870ae49217c6c25a1ab08ef8d900266d08d70c1b
Sagar Vrajalal <sagar7666@gmail.com>
2018-03-11 16:07:33 +0530

    clean up win conditions and add board full check

--- main.cpp
+++ main.cpp
@@ -12,25 +12,31 @@ int main() {
     if (board.getStart()) {
       std::cout << "You play first! Good luck." << '\n';
       board.drawBoard();
-//      while(!board.winCondition() && !board.boardFull()) {
+      while(!board.boardFull()) {
         board.userMove();
         board.drawBoard();
+        if(board.winCondition()) break;
         board.botThink();
         board.botMove();
         board.drawBoard();
+        if(board.winCondition()) break;
         decision = "NULL";
-//      }
+      }
     } else {
        std::cout << "Bot plays first! Good luck." << '\n';
-//       while(!board.winCondition() && !board.boardFull()) {
-         board.drawBoard();
+       board.drawBoard();
+       while(!board.boardFull()) {
          board.botThink();
          board.botMove();
          board.drawBoard();
+         if(board.winCondition()) break;
          board.userMove();
+         board.drawBoard();
+         if(board.winCondition()) break;
          decision = "NULL";
-//      }
+      }
     }
   }
+
   return 0;
 }
